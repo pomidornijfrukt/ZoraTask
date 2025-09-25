e88cd1e6795efb281610f2c7772c79cd1f4db555
Robert <robert@rvt.lv>
2025-09-25 10:48:39 +0300

    ADDED BIOME

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,9 +1,9 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db/db";
+import { betterAuth } from "better-auth"
+import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { db } from "@/lib/db"
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
-    }),
-});
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+})

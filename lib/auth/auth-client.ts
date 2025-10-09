8c0eab32c9b6e8d51abbc1e4d7512351d9266635
Robert Potemkin <rp00549@rvt.lv>
2025-10-09 21:10:56 +0300

    Add space between ] and }

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -1,4 +1,4 @@
 import { organizationClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 
-export const authClient = createAuthClient({ plugins: [organizationClient()]})
+export const authClient = createAuthClient({ plugins: [organizationClient()] })
